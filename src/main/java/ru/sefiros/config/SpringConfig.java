50ad59c3afe5f8631559ed93b12d6cedfcf0e0df
Viacheslav Lopushko <s-slim@ya.ru>
2022-02-13 18:21:22 +0300

    Add character encoding rule (fix russian characters displaying)

--- src/main/java/ru/sefiros/config/SpringConfig.java
+++ src/main/java/ru/sefiros/config/SpringConfig.java
@@ -52,6 +52,7 @@ public class SpringConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
         registry.viewResolver(resolver);
     }
 
